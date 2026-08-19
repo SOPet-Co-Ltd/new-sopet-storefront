6095c821492f307692b6f39ad3f9058284464506
Tanatorn Suteerapornchai <132776555+atnts@users.noreply.github.com>
2026-08-19 13:58:52 +0700

    feat: mock next/image for improved test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,7 +2,26 @@ import '@testing-library/jest-dom/vitest';
 import { resetApolloClientSingletons } from '@apollo/client-integration-nextjs';
 import { cleanup } from '@testing-library/react';
 import { afterAll, afterEach, beforeAll, vi } from 'vitest';
+import React from 'react';
 import { server } from './mocks/server';
+
+vi.mock('next/image', () => ({
+  default: ({
+    src,
+    alt,
+    priority: _priority,
+    ...props
+  }: {
+    src: string | { src: string };
+    alt: string;
+    priority?: boolean;
+    [key: string]: unknown;
+  }) => {
+    const imgSrc =
+      typeof src === 'object' && src !== null && 'src' in src ? (src as { src: string }).src : src;
+    return React.createElement('img', { src: imgSrc, alt, ...props });
+  },
+}));
 
 beforeAll(() => {
   Object.defineProperty(window, 'matchMedia', {
